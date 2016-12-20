635b9d43ac8c081f241226296ba0d5d460ec59d0
Greg Wiley <gwiley@cj.com>
2016-12-19 17:35:51 -0800

    diagonal

--- src/Main.purs
+++ src/Main.purs
@@ -1,9 +1,12 @@
 module Main where
 
+import Math (sqrt)
 import Prelude
 import Control.Monad.Eff (Eff)
-import Control.Monad.Eff.Console (CONSOLE, log)
+import Control.Monad.Eff.Console (CONSOLE, logShow)
+
+diagonal :: Number -> Number -> Number
+diagonal w h = sqrt (w * w + h * h)
 
 main :: forall e. Eff (console :: CONSOLE | e) Unit
-main = do
-  log "Hello sailor!"
+main = logShow $ diagonal 3.0 4.0
